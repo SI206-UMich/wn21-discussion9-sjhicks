8c0bcc4317d0d92cb770f88c858bd411c41cc107
Samantha Hicks <sjhicks@umich.edu>
2021-03-22 16:57:26 -0400

    final

--- diss_9.py
+++ diss_9.py
@@ -6,25 +6,47 @@ import unittest
 # Task 1: Get the URL that links to the Pokemon Charmander's webpage.
 # HINT: You will have to add https://pokemondb.net to the URL retrieved using BeautifulSoup
 def getCharmanderLink(soup):
-    pass
+    anchor = soup.find("div", class_= 'infocard-list infocard-list-pkmn-lg')
+    anchor2 = anchor.find_all('span', class_ = 'infocard-lg-img')[3]
+    anchor3 = anchor2.find('a')['href']
+    return "https://pokemondb.net" + anchor3
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
 #         them into a list. The function should return that list of moves.
 def getEggMoves(pokemon):
     url = 'https://pokemondb.net/pokedex/'+pokemon
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
     #add code here
+    moves = []
+    anchor = soup.find_all('table', class_ = 'data-table')[2]
+    anchor2 = anchor.find_all('a', class_ = 'ent-name')
+    for move in anchor2:
+        new = move.text
+        moves.append(new)
+    return moves
 
+
+    
 # Task 3: Create a regex expression that will find all the times that have these formats: @2pm @5 pm @10am
 # Return a list of these times without the '@' symbol. E.g. ['2pm', '5 pm', '10am']
 def findLetters(sentences):
     # initialize an empty list
-    
+    letters = []
+
 
     # define the regular expression
-    
+    rgx = r'\@(\d{1,2} ?[pm|am]+)'
 
     # loop through each sentence or phrase in sentences
-    
+    #lines = read_file(sentences) 
+    for sentence in sentences:
+        x = re.findall(rgx,sentence)
+        for i in x:
+            letters.append(i)
+   
+    return letters
+
 
     # find all the words that match the regular expression in each sentence
        
@@ -33,7 +55,7 @@ def findLetters(sentences):
 
 
     #return the list of the last letter of all words that begin or end with a capital letter
-
+   
 
 
 def main():
